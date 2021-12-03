f07c2b436829ff1433e131f05b3738eed40b8211
akashdeep007 <beingakscool@gmail.com>
2021-12-03 20:16:54 +0530

    To get config from properties file

--- src/main/java/com/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,16 +1,20 @@
 package com.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservices.limitsservice.configs.Configuration;
 import com.microservices.limitsservice.entity.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration configuration;
+
 	@GetMapping("/limits")
-	public LimitConfiguration retrieveLimits()
-	{
-		return new LimitConfiguration(10,1);
+	public LimitConfiguration retrieveLimits() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
